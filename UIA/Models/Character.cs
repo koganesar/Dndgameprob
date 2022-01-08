7099f61a7bd206f2400b283288e0c747a19511e7
Mansur <ahmatowmansur@gmail.com>
2022-01-08 19:25:34 +0300

    q

---
+++ UIA/Models/Character.cs
@@ -0,0 +1,13 @@
+namespace UIA.Models
+{
+    public class Character
+    {
+        public string UserName { get; set; }
+        public int HitPoints { get; set; }
+        public int AttackModifier { get; set; }
+        public int AttackPerRound { get; set; }
+        public int Damage { get; set; }
+        public int Weapon { get; set; }
+        public int AC { get; set; }
+    }
+}
