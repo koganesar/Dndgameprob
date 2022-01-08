c18dd0b05afb87f8f0308933c1dd4b99fb28041e
Mansur <ahmatowmansur@gmail.com>
2022-01-08 19:00:21 +0300

    qwe

---
+++ Ui/Models/Character.cs
@@ -0,0 +1,13 @@
+namespace UserInterface.Models
+{
+    public class Character
+    {
+        public string UserName { get; set; }
+        public int HitPoints { get; set; }
+        public int AttackModifier { get; set; }
+        public int AttackPerRound { get; set; }
+        public int Damage { get; set; }
+        public int Weapon { get; set; }
+        public int AC { get; set; }
+    }
+}
--- UserInterface/Models/Character.cs
+++ UserInterface/Models/Character.cs
@@ -2,12 +2,6 @@ namespace UserInterface.Models
 {
     public class Character
     {
-        public string UserName { get; set; }
-        public int HitPoints { get; set; }
-        public int AttackModifier { get; set; }
-        public int AttackPerRound { get; set; }
-        public int Damage { get; set; }
-        public int Weapon { get; set; }
-        public int AC { get; set; }
+        
     }
 }
